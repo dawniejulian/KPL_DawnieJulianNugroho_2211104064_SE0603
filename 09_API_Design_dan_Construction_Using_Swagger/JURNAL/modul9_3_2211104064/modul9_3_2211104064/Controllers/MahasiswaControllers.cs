2a8769e978cbe708840f54f905679d822a049726
dawniejulian <dawniejulian@gmail.com>
2025-06-13 01:40:42 +0700

    menambahkan jurnal 9.3

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/modul9_3_2211104064/modul9_3_2211104064/Controllers/MahasiswaControllers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using modul9_3_2211104064.Models;
+
+namespace modul9_3_2211104064.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa { Name = "Julian", Nim = "2211104064", Course = new List<string>{"PBO", "KPL"}, Year = 2022 },
+            new Mahasiswa { Name = "Fathur", Nim = "2211104070", Course = new List<string>{"PBO", "AI"}, Year = 2022 },
+            new Mahasiswa { Name = "Samud", Nim = "2211104062", Course = new List<string>{"Web", "Basis Data"}, Year = 2022 }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> GetAll()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> GetById(int id)
+        {
+            if (id < 0 || id >= mahasiswaList.Count)
+                return NotFound();
+            return mahasiswaList[id];
+        }
+
+        [HttpPost]
+        public ActionResult AddMahasiswa(Mahasiswa mhs)
+        {
+            mahasiswaList.Add(mhs);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMahasiswa(int id)
+        {
+            if (id < 0 || id >= mahasiswaList.Count)
+                return NotFound();
+            mahasiswaList.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
+
---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/modul9_3_2211104064/modul9_3_2211104064/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace modul9_3_2211104064;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
